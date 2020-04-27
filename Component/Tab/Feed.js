43de45f47cfbe6fc65f80d467df370e3d9351e74
yeoneum <52909529+timevoltex@users.noreply.github.com>
2020-04-27 17:43:13 +0900

    20.04.27 Feed 탭 구현
    native-base의 Tab 부분을 이용하여 움직이는 탭 구현
    (첫 슬라이드시 랜더링 안되는 버그가 있음. 버튼누르면 해결됨)

--- Component/Tab/Feed.js
+++ Component/Tab/Feed.js
@@ -1,13 +1,66 @@
-import React from 'react'
-import {Container, Content} from 'native-base';
-import {Text} from 'react-native'
+import React, {useState} from "react";
+import { View, StyleSheet, Text, Dimensions, Image } from "react-native";
+import {
+  Icon,
+  Container,
+  Header,
+  Input,
+  Item,
+  Content,
+  Segment,
+  Tab,
+  Tabs,
+  TabHeading,
+  Thumbnail,
+  Card,
+  CardItem,
+  Body,
+  Left,
+  Right,
+  Button,
+  Title,
+} from "native-base";
+import Carousel from "react-native-snap-carousel";
+import InstaTab from './Feed/InstaTab';
+import PhotoTab from './Feed/PhotoTab';
+
+const { width: viewportWidth, height: viewportHeight } = Dimensions.get(
+  "window"
+);
 
 const Feed = () => {
+
   return(
   <Container>
-    <Content>
-      <Text>This is Feed Page</Text>
-    </Content>
+    <Header>
+        <Left>
+          <Icon type="Entypo" name="location-pin" style={{ paddingLeft: 10 }} />
+        </Left>
+        <Body>
+          <Title>현재 주소</Title>
+        </Body>
+        <Right>
+          <Icon
+            type="FontAwesome"
+            name="sliders"
+            style={{ paddingRight: 10 }}
+          />
+        </Right>
+      </Header>
+    <Header hasTabs searchBar rounded style={{ backgroundColor: "#fff" }}>
+        <Item>
+          <Icon name="ios-search" />
+          <Input placeholder="오늘은 뭘 먹지?" />
+        </Item>
+      </Header>
+      <Tabs>
+        <Tab heading={<TabHeading><Icon name="square"/></TabHeading>}>
+          <InstaTab/>
+        </Tab>
+        <Tab heading={<TabHeading><Icon name="apps"/></TabHeading>}>
+          <PhotoTab/>
+        </Tab>
+      </Tabs>
   </Container>
   )
 }
