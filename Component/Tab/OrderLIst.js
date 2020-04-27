d9c498d70f04950d021380417a39dc4d696a2864
yeoneum <52909529+timevoltex@users.noreply.github.com>
2020-04-27 17:44:08 +0900

    20.04.27 헤더 생성
    OrderList 탭에 헤더를 생성함

--- Component/Tab/OrderLIst.js
+++ Component/Tab/OrderLIst.js
@@ -1,10 +1,25 @@
 import React from 'react'
-import {Container, Content, Header} from 'native-base';
-import {Text} from 'react-native'
+import {Container, Content, Header, Left, Icon, Body, Title, Right} from 'native-base';
+import {Text, Image} from 'react-native'
 
 const OrderList = () => {
   return(
   <Container>
+    <Header>
+        <Left>
+          <Icon type="Entypo" name="location-pin" style={{ paddingLeft: 10 }} />
+        </Left>
+        <Body>
+          <Title>현재 주소</Title>
+        </Body>
+        <Right>
+          <Icon
+            type="FontAwesome"
+            name="sliders"
+            style={{ paddingRight: 10 }}
+          />
+        </Right>
+      </Header>
     <Content>
       <Text>This is OrderList Page</Text>
     </Content>
