6f97aa929a6bcc16df61b62fea70aed0d2425f22
yeoneum <52909529+timevoltex@users.noreply.github.com>
2020-04-28 18:12:09 +0900

    20.04.28 reordering Code

--- Component/Tab/OrderLIst.js
+++ Component/Tab/OrderLIst.js
@@ -1,14 +1,25 @@
-import React from 'react'
-import {Container, Content, Header, Left, Icon, Body, Title, Right} from 'native-base';
-import {Text, Image} from 'react-native'
+import React from "react";
+import {
+  Container,
+  Content,
+  Header,
+  Left,
+  Icon,
+  Body,
+  Title,
+  Right,
+  Item,
+  Input,
+  Card,
+  CardItem,
+} from "native-base";
+import { Text, Image } from "react-native";
 
 const OrderList = () => {
-  return(
-  <Container>
-    <Header>
-        <Left>
-          <Icon type="Entypo" name="location-pin" style={{ paddingLeft: 10 }} />
-        </Left>
+  return (
+    <Container>
+      <Header>
+        <Left></Left>
         <Body>
           <Title>현재 주소</Title>
         </Body>
@@ -20,10 +31,26 @@ const OrderList = () => {
           />
         </Right>
       </Header>
-    <Content>
-      <Text>This is OrderList Page</Text>
-    </Content>
-  </Container>
-  )
-}
+      <Header searchBar rounded style={{ backgroundColor: "#fff" }}>
+        <Item>
+          <Icon name="ios-search" />
+          <Input placeholder="오늘은 뭘 먹지?" />
+        </Item>
+      </Header>
+      <Content padder>
+        <Card>
+          <CardItem style={{ flexDirection: "column", alignItems: "baseline" }}>
+            <Text>시간</Text>
+            <Text>가게명</Text>
+            <Text style={{display:'flex', justifyContent:'space-between', width:'100%'}}> 
+              <Text>메뉴</Text>
+              <Text>00,000원</Text>
+            </Text>
+          </CardItem>
+          <CardItem style={{borderTopWidth:0.5}}><Text style={{width:'100%', textAlign:'center'}}>리뷰쓰기</Text></CardItem>
+        </Card>
+      </Content>
+    </Container>
+  );
+};
 export default OrderList;
