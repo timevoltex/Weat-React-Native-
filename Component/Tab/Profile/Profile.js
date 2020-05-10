2c97e45165b0a2ec719ec559b2f9db73ed2b4b59
timevoltex <buja801@daum.net>
2020-05-10 14:08:38 +0900

    20.05.10 스타일 변경
    1.  props에 navigation 전달을 위한 navigation prop 설정
    2. br태그 -> \n으로 줄바꿈 변경
    3. 탭 부분 스타일 변경(아직 수정 예정)

--- Component/Tab/Profile/Profile.js
+++ Component/Tab/Profile/Profile.js
@@ -23,11 +23,11 @@ import Noti from './Noti';
 import Analyze from './analyze';
 import {AllIcon} from '../ImportImage';
 
-const Profile = () => {
+const Profile = ({navigation}) => {
   const [activeTab, setActiveTab] = useState(0);
   return (
     <Container>
-      <Header style={{backgroundColor:'transparent'}}>
+      <Header hasTabs style={{backgroundColor:'transparent'}}>
         <Left style={{flex:1,}}>
           <Image source={AllIcon.editInactive} style={{width:50, height:50}}/>
         </Left>
@@ -38,7 +38,6 @@ const Profile = () => {
           <Image source={AllIcon.settingsInactive} style={{width:50, height:50}} />
         </Right>
       </Header>
-      <Content>
         <Card>
           <CardItem>
           <Left>
@@ -56,16 +55,16 @@ const Profile = () => {
           </CardItem>
           <CardItem>
             <View style={{flexDirection:'row', justifyContent:'space-around', width:'100%'}}>
-              <Text style={{textAlign:'center'}}>000<br/>팔로워</Text>
-              <Text style={{textAlign:'center'}}>000<br/>팔로잉</Text>
-              <Text style={{textAlign:'center'}}>000<br/>리뷰</Text>
-              <Text style={{textAlign:'center'}}>000<br/>단골식당</Text>
+              <Text style={{textAlign:'center'}}>000{"\n"}팔로워</Text>
+              <Text style={{textAlign:'center'}}>000{"\n"}팔로잉</Text>
+              <Text style={{textAlign:'center'}}>000{"\n"}리뷰</Text>
+              <Text style={{textAlign:'center'}}>000{"\n"}단골식당</Text>
             </View>
           </CardItem>
         </Card>
-        <Tabs tabBarUnderlineStyle={tab.tabUnderline} initialPage={activeTab} onChangeTab={({i}) => setActiveTab(i)}>
-          <Tab heading={<TabHeading style={tab.tabHeader}><Text style={activeTab === 0 ? tab.activeText : tab.inactiveText}>프로필</Text></TabHeading>}>
-            <MyInsta />
+        <Tabs style={{backgroundColor:'transparent'}} tabBarUnderlineStyle={tab.tabUnderline} initialPage={activeTab} onChangeTab={({i}) => setActiveTab(i)}>
+          <Tab tabStyle={{backgroundColor:'transparent'}} heading={<TabHeading style={tab.tabHeader}><Text style={activeTab === 0 ? tab.activeText : tab.inactiveText}>프로필</Text></TabHeading>}>
+            <MyInsta props={navigation} style={{overflow:"scroll"}}/>
           </Tab>
           <Tab heading={<TabHeading style={tab.tabHeader}><Text style={activeTab === 1 ? tab.activeText : tab.inactiveText}>알림</Text></TabHeading>}>
             <Noti/>
@@ -73,7 +72,6 @@ const Profile = () => {
           <Tab heading={<TabHeading style={tab.tabHeader}><Text style={activeTab === 2 ? tab.activeText : tab.inactiveText}>분석</Text></TabHeading>}><Analyze/></Tab>
           <Tab heading={<TabHeading style={tab.tabHeader}><Text style={activeTab === 3 ? tab.activeText : tab.inactiveText}>컬렉션</Text></TabHeading>}><MyPhoto /></Tab>
         </Tabs>
-      </Content>
   
     </Container>
   );
