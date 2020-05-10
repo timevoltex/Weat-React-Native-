b5c4fd74e1bb2d721aad6f240405c795365c3003
timevoltex <buja801@daum.net>
2020-05-10 18:37:04 +0900

    20.05.10 스크린페이지 수정
    TimeSale 대신 OrderPage를 추가함

--- Component/Tab/Home/index.js
+++ Component/Tab/Home/index.js
@@ -1,14 +1,14 @@
 import React, { Component } from "react";
 import HomeTab from "./HomeTab.js";
 import StoreDetail from "./StoreDetail.js";
-import TimeSale from './TimeSale';
+import OrderPage from './OrderPage';
 import EventTab from './EventTab';
 import { createStackNavigator } from "react-navigation-stack";
 import {createAppContainer} from 'react-navigation'
 const DrawNav = createStackNavigator({
   HomeTab: { screen: HomeTab, navigationOptions:{header:null}},
   StoreDetail: { screen: StoreDetail, navigationOptions:{header:null} },
-  TimeSale: { screen: TimeSale, navigationOptions:{header:null} },
+  OrderPage: { screen: OrderPage, navigationOptions:{header:null} },
   EventTab: {screen: EventTab, navigationOptions:{header:null}}
 });
 export default createAppContainer(DrawNav);
