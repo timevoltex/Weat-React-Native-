a50b90959775a077e3c40c6457e604f211236c5b
yeoneum <52909529+timevoltex@users.noreply.github.com>
2020-05-11 17:48:18 +0900

    20.05.11 코드 수정
    1. 불필요 코드 제거
    2. 네이버 맵추가를 위한 예제 코드 복붙-> import에서 문제가 있는지 작동하지 않음 내일 수정 예정
    3. 공통 css를 가져와서 적용하였음
    4. EventTab에서 받아온 props들을 이용하여 동적으로 변하도록 수정
    5. 전체적으로 css를 부여함

--- Component/Tab/Home/StoreDetail.js
+++ Component/Tab/Home/StoreDetail.js
@@ -13,30 +13,48 @@ import {
   Footer,
   Badge,
   Fab,
+  Icon,
 } from "native-base";
 import { Image, StyleSheet, View, Dimensions, Text } from "react-native";
-import { AllIcon, Back, List as ListIcon } from "../ImportImage";
+import NaverMapView, {
+  Circle,
+  Marker,
+  Path,
+  Polygon,
+  Polyline,
+} from "react-native-nmap";
+import { Back, Diner } from "../ImportImage";
+import { headerStyle } from "./HomeTab";
 
 const { width: viewportWidth, height: viewportHeight } = Dimensions.get(
   "window"
 );
 
 function StoreDetail({ navigation }) {
+  const P0 = { latitude: 37.564362, longitude: 126.977011 };
+  const P1 = { latitude: 37.565051, longitude: 126.978567 };
+  const P2 = { latitude: 37.565383, longitude: 126.976292 };
   return (
     <Container>
-      <Header style={style.header}>
-        <Left style={style.headerLeft}>
-          <Button style={{ paddingLeft: 0 }} transparent onPress={() => navigation.goBack()}>
-            <Back fill="#c2c2c2" style={style.icon} />
+      <Header style={headerStyle.header}>
+        <Left style={headerStyle.headerLeft}>
+          <Button
+            style={headerStyle.headerBack}
+            transparent
+            onPress={() => navigation.goBack()}
+          >
+            <Back fill="#c2c2c2" width={24} height={18} />
           </Button>
         </Left>
-        <Body style={{ alignItems: "center" }}>
-          <Title style={style.headertitle}>상호명</Title>
+        <Body style={headerStyle.headerBody}>
+          <Title style={[headerStyle.headertitle, { fontSize: 16 }]}>
+            {navigation.getParam("store")}
+          </Title>
         </Body>
         <Right>
-          <ListIcon
+          <Diner
             fill="#c2c2c2"
-            style={{ height: 50, width: 50, paddingRight: 10 }}
+            style={{ height: 22, width: 22, paddingRight: 10 }}
           />
         </Right>
       </Header>
@@ -49,7 +67,6 @@ function StoreDetail({ navigation }) {
           }}
         />
         <View style={{ padding: 17 }}>
-          <Text>시간</Text>
           <Text
             style={{
               display: "flex",
@@ -57,8 +74,19 @@ function StoreDetail({ navigation }) {
               width: "100%",
             }}
           >
-            <Text>메뉴</Text>
-            <Text>구매 인원</Text>
+            <Text style={{ fontSize: 14, color: "#707070" }}>
+              {navigation.getParam("time")}
+            </Text>
+            <Icon style={{ color: "#CCC", fontSize: 14 }} name="md-share" />
+          </Text>
+          <Text style={{ fontWeight: "bold", fontSize: 22 }}>
+            {navigation.getParam("menu")}
+          </Text>
+          <Text style={{ fontSize: 16, color: "#707070" }}>
+            {navigation.getParam("store")}
+          </Text>
+          <Text style={{ fontSize: 14, color: "#707070" }}>
+            {navigation.getParam("addr")}
           </Text>
           <Text
             style={{
@@ -67,29 +95,88 @@ function StoreDetail({ navigation }) {
               width: "100%",
             }}
           >
-            <Text>할인율 가격</Text>
-            <Text>잔여 물량</Text>
+            <Text
+              style={{ fontSize: 14, fontWeight: "bold", color: "#707070" }}
+            >
+              {navigation.getParam("sale")}%{" "}
+              <Text
+                style={{
+                  fontWeight: "regular",
+                  textDecorationLine: "line-through",
+                  color: "#c2c2c2",
+                  textDecorationColor: "#c2c2c2",
+                }}
+              >
+                {navigation.getParam("price")}
+              </Text>
+            </Text>
+            <Text>{navigation.getParam("cnt")}개 남음</Text>
+          </Text>
+          <Text
+            style={{
+              display: "flex",
+              justifyContent: "space-between",
+              width: "100%",
+            }}
+          >
+            <Text
+              style={{ fontSize: 20, fontWeight: "bold", color: "#9943fc" }}
+            >
+              {navigation.getParam("saleprice")}원
+            </Text>
+            <Badge
+              style={{
+                alignItems: "center",
+                backgroundColor: "#9943fc",
+                width: 41,
+                height: 21,
+              }}
+            >
+              <Text style={{ fontSize: 14, color: "white" }}>
+                {navigation.getParam("remain")}분
+              </Text>
+            </Badge>
           </Text>
         </View>
         <View style={{ backgroundColor: "#f2f2f2" }}>
           <List>
             <ListItem itemHeader first>
-              <Text>상세정보</Text>
-            </ListItem>
-            <ListItem >
-              <Text>영업시간</Text>
-            </ListItem>
-            <ListItem>
-              <Text>가게위치</Text>
+              <Text
+                style={{ fontSize: 16, fontWeight: "bold", color: "#707070" }}
+              >
+                상세정보
+              </Text>
             </ListItem>
             <ListItem>
-              <Text>전화번호</Text>
+              <Text style={[style.storeDetail, { fontWeight: "bold" }]}>
+                영업시간{" "}
+              </Text>
+              <Text style={[style.storeDetail, { fontWeight: "regular" }]}>
+                {" "}
+                {navigation.getParam("time")}
+              </Text>
             </ListItem>
             <ListItem>
-              <Text>해시태그</Text>
+              <Text style={[style.storeDetail, { fontWeight: "bold" }]}>
+                가게위치{" "}
+              </Text>
+              <Text style={[style.storeDetail, { fontWeight: "regular" }]}>
+                {" "}
+                {navigation.getParam("addr")}
+              </Text>
+            </ListItem>
+            <ListItem>
+              <Text style={[style.storeDetail, { fontWeight: "bold" }]}>
+                전화번호{" "}
+              </Text>
+            </ListItem>
+            <ListItem>
+              <Text style={[style.storeDetail, { fontWeight: "bold" }]}>
+                해시태그{" "}
+              </Text>
             </ListItem>
             <ListItem last>
-              <Text>
+              <Text style={{ fontSize: 14, color: "#a2a2a2" }}>
                 Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
                 diam nonumy eirmod tempor invidunt ut labore et dolore magna
                 aliquyam erat, sed diam voluptua. At vero eos et accusam et
@@ -99,37 +186,105 @@ function StoreDetail({ navigation }) {
               </Text>
             </ListItem>
             <ListItem itemHeader first>
-              <Text>위치</Text>
+              <Text style={{ fontSize: 16, color: "#707070" }}>위치</Text>
             </ListItem>
             <ListItem last>
-              <View
-                style={{
-                  width: viewportWidth - 36,
-                  height: viewportWidth - 36,
-                  backgroundColor: "#DCDCDC",
-                }}
+              <NaverMapView
+                style={{ width: "100%", height: "100%" }}
+                showsMyLocationButton={true}
+                center={{ ...P0, zoom: 16 }}
+                onTouch={(e) =>
+                  console.warn("onTouch", JSON.stringify(e.nativeEvent))
+                }
+                onCameraChange={(e) =>
+                  console.warn("onCameraChange", JSON.stringify(e))
+                }
+                onMapClick={(e) =>
+                  console.warn("onMapClick", JSON.stringify(e))
+                }
               >
-                <Text>naverMap 추가 예정</Text>
-              </View>
+                <Marker
+                  coordinate={P0}
+                  onClick={() => console.warn("onClick! p0")}
+                />
+                <Marker
+                  coordinate={P1}
+                  pinColor="blue"
+                  onClick={() => console.warn("onClick! p1")}
+                />
+                <Marker
+                  coordinate={P2}
+                  pinColor="red"
+                  onClick={() => console.warn("onClick! p2")}
+                />
+                <Path
+                  coordinates={[P0, P1]}
+                  onClick={() => console.warn("onClick! path")}
+                  width={10}
+                />
+                <Polyline
+                  coordinates={[P1, P2]}
+                  onClick={() => console.warn("onClick! polyline")}
+                />
+                <Circle
+                  coordinate={P0}
+                  color={"rgba(255,0,0,0.3)"}
+                  radius={200}
+                  onClick={() => console.warn("onClick! circle")}
+                />
+                <Polygon
+                  coordinates={[P0, P1, P2]}
+                  color={`rgba(0, 0, 0, 0.5)`}
+                  onClick={() => console.warn("onClick! polygon")}
+                />
+              </NaverMapView>
             </ListItem>
             <ListItem itemHeader first>
-              <Text>타임세일 이용방법</Text>
+              <Text style={{ fontSize: 16, color: "#707070" }}>
+                타임세일 이용방법
+              </Text>
             </ListItem>
             <ListItem last>
-              <Text>
+              <Text style={{ fontSize: 14, color: "#a2a2a2" }}>
                 Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
                 diam nonumy eirmod tempor invidunt ut labore et dolore magna
                 aliquyam erat, sed diam voluptua. At vero eos et accusam et
                 justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea
                 takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum
                 dolor sit amet, consetetur
-                </Text>
+              </Text>
             </ListItem>
           </List>
         </View>
-        <Button onPress={() => navigation.navigate('OrderPage')}
-          style={[style.boxShadow,{ position: 'sticky', width: '90%', height: 50, borderRadius: 30, backgroundColor: '#9c48fc', bottom: 10, left: '5%' }]}>
-          <Text style={{alignSelf:'center', marginHorizontal:'auto', color:'white', fontSize:20}}>예약하기</Text>
+        <Button
+          onPress={() =>
+            navigation.navigate("OrderPage", {
+              price: navigation.getParam("price"),
+            })
+          }
+          style={[
+            style.boxShadow,
+            {
+              position: "sticky",
+              width: "90%",
+              height: 50,
+              borderRadius: 30,
+              backgroundColor: "#9c48fc",
+              bottom: 10,
+              left: "5%",
+            },
+          ]}
+        >
+          <Text
+            style={{
+              alignSelf: "center",
+              marginHorizontal: "auto",
+              color: "white",
+              fontSize: 20,
+            }}
+          >
+            예약하기
+          </Text>
         </Button>
       </Content>
     </Container>
@@ -164,15 +319,19 @@ const style = StyleSheet.create({
   iconRight: {
     paddingRight: 10,
   },
+  storeDetail: {
+    color: "#707070",
+    fontSize: 14,
+  },
   boxShadow: {
     shadowColor: "#000",
     shadowOffset: {
-      width: -5,  //x-position
-      height: 5,  //y-position
+      width: -5, //x-position
+      height: 5, //y-position
     },
     shadowOpacity: 0.4, //opacity
     shadowRadius: 4.65, //blur
 
     elevation: 7, // This adds a drop shadow to the item and affects z-order for overlapping views
-  }
+  },
 });
